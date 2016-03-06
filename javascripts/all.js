9337daaa0eda3f1bbe6188957945a3b06af62175
Krzysztof Rutka <krzysztof.rutka@gmail.com>
2016-03-06 17:19:06 +0100

    Deploy :rocket:

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,18 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+    var quotes = document.getElementsByClassName("quotes");
+
+    var quotes1 = quotes[0];
+    var idxs = Array(quotes1.children.length).fill().map(function (x, i) {
+        return i;
+    }); // [0,1,2,3,4...13]
+
+    var idx1 = idxs[Math.floor(Math.random() * idxs.length)];
+    quotes1.children[idx1].className = "quote-visible";
+    idxs.splice(idx1, 1);
+
+    var quotes2 = quotes[1];
+    var idx2 = idxs[Math.floor(Math.random() * idxs.length)];
+    quotes2.children[idx2].className = "quote-visible";
+}, false);
